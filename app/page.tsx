7a1760bfa98f2ba4accc3b55840d1aff3a933226
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-11 16:34:43 +0530

    used link from next for routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = () => {
   return (
     <>
       <h1 className="text-3xl">HomePage</h1>
-      <a href="/properties">Go to properties</a>
+      <Link href="/properties">Go to properties</Link>
     </>
   );
 };
