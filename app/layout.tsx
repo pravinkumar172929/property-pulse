678079954c58a60e3d809fd29c9f166197ef2b1d
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-11 16:01:03 +0530

    added title using metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import "@/assets/styles/globals.css";
 import type { RootLayoutProps } from "./types";
 
+export const metadata = {
+  title: "PropertyPulse | Find the Perfect Rental",
+};
+
 const MainLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
