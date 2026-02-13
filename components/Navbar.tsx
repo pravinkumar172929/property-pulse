5c5abfe5a219309c061412420b046c170f253453
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-13 13:06:01 +0530

    made navbar client and useState to toggle on btn click

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+"use client";
 import Image from "next/image";
 import logo from "@/assets/images/logo.png";
 import profileDefault from "@/assets/images/profile.png";
 import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMobileviewOpen, setIsMobileviewOpen] = useState(false);
+  console.log("isMobileviewOpen => ", isMobileviewOpen);
+
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -18,6 +22,7 @@ const Navbar = () => {
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
+              onClick={() => setIsMobileviewOpen((prev) => !prev)}
             >
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
@@ -196,7 +201,7 @@ const Navbar = () => {
             Add Property
           </Link>
           <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-            <i className="fa-brands fa-google mr-2"></i>
+            <FaGoogle className="text-white -mr-2" />
             <span>Login or Register</span>
           </button>
         </div>
