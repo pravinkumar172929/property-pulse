97e7f04be335687ef5acc9c56e7189fd0a9333bc
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-13 19:47:17 +0530

    show hide add property on loggen in condition

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,8 +11,7 @@ const Navbar = () => {
   const pathname = usePathname();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-  //   console.log("isMobileviewOpen => ", isMobileMenuOpen);
-  console.log("isProfileMenuOpen => ", isProfileMenuOpen);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
@@ -75,14 +74,16 @@ const Navbar = () => {
                 >
                   Properties
                 </Link>
-                <Link
-                  href="/properties/add"
-                  className={`${
-                    pathname === "/properties/add" ? "bg-black" : ""
-                  } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
-                >
-                  Add Property
-                </Link>
+                {isLoggedIn && (
+                  <Link
+                    href="/properties/add"
+                    className={`${
+                      pathname === "/properties/add" ? "bg-black" : ""
+                    } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                  >
+                    Add Property
+                  </Link>
+                )}
               </div>
             </div>
           </div>
@@ -214,14 +215,17 @@ const Navbar = () => {
             >
               Properties
             </Link>
-            <Link
-              href="/properties/add"
-              className={`${
-                pathname === "/properties/add" ? "bg-black" : ""
-              } text-white block rounded-md px-3 py-2 text-base font-medium`}
-            >
-              Add Property
-            </Link>
+            {isLoggedIn && (
+              <Link
+                href="/properties/add"
+                className={`${
+                  pathname === "/properties/add" ? "bg-black" : ""
+                } text-white block rounded-md px-3 py-2 text-base font-medium`}
+              >
+                Add Property
+              </Link>
+            )}
+
             <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
               <FaGoogle className="text-white -mr-2" />
               <span>Login or Register</span>
