6ca72a34dbb95fb78f0ff42d4c9bfaf80404e044
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-14 23:10:06 +0530

    removed google icon on mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -231,7 +231,7 @@ const Navbar = () => {
             )}
 
             <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5">
-              <FaGoogle className="text-white -mr-2" />
+              {/* <FaGoogle className="text-white -mr-2" /> */}
               <span>Login or Register</span>
             </button>
           </div>
