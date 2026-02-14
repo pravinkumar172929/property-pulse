dc83be67433ee45e06ffaecd59a300b356fcc258
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-14 23:17:25 +0530

    added hero component skelton

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
-import React from "react";
+import Hero from "@/components/Hero";
 
 const HomePage = () => {
   return (
     <>
-      <h1 className="text-3xl">HomePage</h1>
-      <Link href="/properties">Go to properties</Link>
+      <Hero />
     </>
   );
 };
