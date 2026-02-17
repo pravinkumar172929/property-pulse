aad834ed3f8f6d4c427a43d53344e31dde062ddb
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-17 15:08:16 +0530

    added error boundary for page

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -4,11 +4,15 @@ const PropertyPage = () => {
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {properties.map((property) => (
-            <h3 key={property._id}>{property.name}</h3>
-          ))}
-        </div>
+        {properties.length === 0 ? (
+          <p> No Properties Found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <h3 key={property._id}>{property.name}</h3>
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
