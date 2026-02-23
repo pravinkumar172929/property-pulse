9032fd0fbaa3daaa7ee27d3aaa3ef58f7743ee78
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-23 13:44:34 +0530

    used strigify for message and connectDB function

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -1,6 +1,11 @@
+import connectDB from "@/config/database";
+
 export const GET = async () => {
+  connectDB();
   try {
-    return new Response("Hello World!", { status: 200 });
+    return new Response(JSON.stringify({ message: "Hello World!" }), {
+      status: 200,
+    });
   } catch (error) {
     console.log(error);
     return new Response("Something went wrong!", { status: 500 });
