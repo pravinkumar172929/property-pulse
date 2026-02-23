94830782c72a0243e1ad4f314dd84bbe33f9a0e9
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-23 13:45:50 +0530

    removed async and connectDB fun from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,8 @@ import Hero from "@/components/Hero";
 import HomeProperty from "@/components/HomeProperty";
 import InfoBox from "@/components/InfoBox";
 import InfoBoxes from "@/components/InfoBoxes";
-import connectDB from "@/config/database";
 
-const HomePage = async () => {
-  console.log(process.env.MONGODB_URI);
-  await connectDB();
-
+const HomePage = () => {
   return (
     <>
       <Hero />
