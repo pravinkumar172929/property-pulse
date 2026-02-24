7c721c9ca09364910c5718076cb6c089a4504ff6
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-24 14:50:29 +0530

    handled case where domain is not yet present

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,12 +1,18 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
 async function fetchProperties() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    if (!apiDomain) {
+      return [];
+    }
+    const res = await fetch(`${apiDomain}/properties`);
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
     return res.json();
   } catch (error) {
     console.log(error);
+    return [];
   }
 }
 
