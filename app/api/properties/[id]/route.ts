8349f66519f1cde0fab7fca66974e4c803d1367a
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-25 21:25:18 +0530

    fetched single property using NextRequest and params

---
+++ app/api/properties/[id]/route.ts
@@ -0,0 +1,29 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { NextRequest } from "next/server";
+
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  //   console.log("***params", params);
+
+  const { id } = await params;
+  console.log("Params ID:", id);
+  try {
+    await connectDB();
+
+    const property = await Property.findById(id);
+
+    if (!property) {
+      return new Response("No Property Found", { status: 404 });
+    }
+
+    return new Response(JSON.stringify(property), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong!", { status: 500 });
+  }
+};
