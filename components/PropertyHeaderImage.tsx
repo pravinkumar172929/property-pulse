c3b5a244e51cc554d0dfc015263e2c4c7e0a4e3e
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-28 22:11:05 +0530

    additional page side fix

--- components/PropertyHeaderImage.tsx
+++ components/PropertyHeaderImage.tsx
@@ -33,7 +33,7 @@ const PropertyHeaderImage = ({ image }: PropertyHeaderImageProps) => {
       </section>
       <section className="bg-blue-50">
         <div className="container m-auto py-10 px-6">
-          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-[70%_30%] w-full gap-6">
             <main>
               <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
                 <div className="text-gray-500 mb-4">Apartment</div>
