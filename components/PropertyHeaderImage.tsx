a20f293be5f5c5fea849c5375a718557c7b19769
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-02-28 21:48:12 +0530

    added go back button

--- components/PropertyHeaderImage.tsx
+++ components/PropertyHeaderImage.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 import Image from "next/image";
 import { PropertyHeaderImageProps } from "@/app/types";
+import Link from "next/link";
 
 const PropertyHeaderImage = ({ image }: PropertyHeaderImageProps) => {
   return (
-    <section>
-      <div className="container-xl m-auto">
-        <div className="grid grid-cols-1">
-          <Image
-            src={`/images/properties/${image}`}
-            alt=""
-            className="object-cover h-[400px] w-full"
-            width={0}
-            height={0}
-            sizes="100vw"
-            priority={true}
-          />
+    <>
+      <section>
+        <div className="container-xl m-auto">
+          <div className="grid grid-cols-1">
+            <Image
+              src={`/images/properties/${image}`}
+              alt=""
+              className="object-cover h-[400px] w-full"
+              width={0}
+              height={0}
+              sizes="100vw"
+              priority={true}
+            />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <i className="fas fa-arrow-left mr-2"></i> Back to Properties
+          </Link>
+        </div>
+      </section>
+    </>
   );
 };
 
