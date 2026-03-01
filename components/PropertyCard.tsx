20d0e3df7b68d1c99f928540c4d3c42d11a84e83
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-03-01 21:48:33 +0530

    fixed property details link

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -1,6 +1,7 @@
 import { PropertyCardProps } from "@/app/types";
 import { getRateDisplay } from "@/utils";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import {
   FaBed,
@@ -74,12 +75,12 @@ export const PropertyCard = ({ property }: PropertyCardProps) => {
               {property.location.city} {property.location.state}
             </span>
           </div>
-          <a
+          <Link
             href={`properties/${property._id}`}
             className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
