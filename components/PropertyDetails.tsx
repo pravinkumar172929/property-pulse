f1e2ff560e56a4d8d17f061946390705bcb6cb50
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-03-04 18:16:24 +0530

    removed identical conditions using map

--- components/PropertyDetails.tsx
+++ components/PropertyDetails.tsx
@@ -28,36 +28,25 @@ const PropertyDetails = ({ property }: PropertyDetailsProps) => {
           Rates & Options
         </h3>
         <div className="flex flex-col md:flex-row justify-around">
-          <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
-            <div className="text-gray-500 mr-2 font-bold">Nightly</div>
-            <div className="text-2xl font-bold text-blue-500">
-              {property.rates.nightly ? (
-                `$${property.rates.nightly.toLocaleString()}`
-              ) : (
-                <FaTimes className="fa fa-xmark text-red-700" />
-              )}
+          {[
+            { label: "Nightly", value: property.rates.nightly },
+            { label: "Weekly", value: property.rates.weekly },
+            { label: "Monthly", value: property.rates.monthly },
+          ].map(({ label, value }) => (
+            <div
+              key={label}
+              className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0"
+            >
+              <div className="text-gray-500 mr-2 font-bold">{label}</div>
+              <div className="text-2xl font-bold text-blue-500">
+                {value > 0 ? (
+                  `$${value.toLocaleString()}`
+                ) : (
+                  <FaTimes className="text-red-700" />
+                )}
+              </div>
             </div>
-          </div>
-          <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
-            <div className="text-gray-500 mr-2 font-bold">Weekly</div>
-            <div className="text-2xl font-bold text-blue-500">
-              {property.rates.weekly ? (
-                `$${property.rates.weekly.toLocaleString()}`
-              ) : (
-                <FaTimes className="fa fa-xmark text-red-700" />
-              )}
-            </div>
-          </div>
-          <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
-            <div className="text-gray-500 mr-2 font-bold">Monthly</div>
-            <div className="text-2xl font-bold text-blue-500">
-              {property.rates.monthly ? (
-                `$${property.rates.monthly.toLocaleString()}`
-              ) : (
-                <FaTimes className="fa fa-xmark text-red-700" />
-              )}
-            </div>
-          </div>
+          ))}
         </div>
       </div>
 
