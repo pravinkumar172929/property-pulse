5a3ab6d64bd281479d3c95dced94f07fee901ae8
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-03-09 14:05:11 +0530

    added skenlton for authFunction

---
+++ utils/authOptions.ts
@@ -0,0 +1,21 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    // 1. invoke on successful login
+    // 2. modifies the session object
+  },
+};
