b461a72c6fcb6d54b07f842b3452a2821ea61923
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-03-10 14:15:15 +0530

    succesful sign and user creation in db

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
   providers: [
@@ -16,6 +18,31 @@ export const authOptions = {
   ],
   callbacks: {
     // 1. invoke on successful login
+    async signIn({ profile }) {
+      // connect to database
+      await connectDB();
+      //   check if user exists
+      const userExists = await User.findOne({ email: profile.email });
+      //   if not, add user to database, turnacate the name
+      const username = profile.name.slice(0, 20);
+      //   save in db
+      await User.create({
+        email: profile.email,
+        username,
+        image: profile.picture,
+      });
+      //   return true to sign in
+      return true;
+    },
+
     // 2. modifies the session object
+    async session({ session }) {
+      // get user from db
+      const user = User.findOne({ email: session.user.email });
+      //   assign user id to session
+      session.user.id = user._id.toString();
+      //   return session
+      return session;
+    },
   },
 };
