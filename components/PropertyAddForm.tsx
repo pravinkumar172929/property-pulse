0ee0088185b063931d4b894de650453c0ab81a52
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-03-18 12:41:02 +0530

    made component cient and used useState for fields

--- components/PropertyAddForm.tsx
+++ components/PropertyAddForm.tsx
@@ -1,6 +1,33 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const PropertyAddForm = () => {
+  const [fields, setFields] = useState({
+    type: "Apartment",
+    name: "Test Property",
+    description: "",
+    location: {
+      street: "",
+      city: "Test City",
+      state: "Test State",
+      zipcode: "",
+    },
+    beds: "3",
+    baths: "2",
+    square_feet: "1800",
+    amenities: [],
+    rates: {
+      weekly: "",
+      monthly: "2000",
+    },
+    seller_info: {
+      name: "",
+      email: "test@test.com",
+      phone: "",
+    },
+    images: [],
+  });
+
   return (
     <form>
       <h2 className="text-3xl text-center font-semibold mb-6">Add Property</h2>
