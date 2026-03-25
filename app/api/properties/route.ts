6690ba38cb2fe975e596f02fce2ae0955fb57561
Pravin Kumar <pravinkumar29.1729@gmail.com>
2026-03-25 16:42:33 +0530

    logged propertyData in server console

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -22,9 +22,38 @@ export const POST = async (request: Request) => {
 
     const amenities = formData.getAll("amenities");
     const images = formData.getAll("images");
-    console.log("amenities => ", amenities);
-    console.log("images => ", images);
-    console.log(formData.get("name"));
+    // console.log("amenities => ", amenities);
+    // console.log("images => ", images);
+    // console.log(formData.get("name"));
+
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly"),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      images,
+    };
+
+    console.log(propertyData);
 
     return new Response(JSON.stringify({ message: "Success" }), {
       status: 200,
